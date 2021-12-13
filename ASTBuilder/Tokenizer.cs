5d8f2c15ea43bf7df16312d4a6f63de0a47f1407
Dmitry Khmelyk <32043078+Nippleduck@users.noreply.github.com>
2021-12-13 21:52:00 +0200

    builders missing

---
+++ ASTBuilder/Tokenizer.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Utility;
+
+namespace ASTBuilder
+{
+    public enum TokenType
+    {
+        Plus,
+        Minus,
+        Multiply,
+        Division,
+        Operand,
+        RBrace,
+        LBrace,
+        Group,
+        EOF
+    }
+
+    public class Token
+    {
+        public Token(string value, TokenType type, int priority)
+        {
+            this.value = value;
+            this.type = type;
+            this.priority = priority;
+        }
+
+        public readonly string value;
+        public readonly TokenType type;
+        public readonly int priority;
+    }
+
+    public class Tokenizer
+    {
+        public Tokenizer(string expression)
+        {
+            var formated = StringFormater.RemoveSpaces(expression);
+            iterator = new Iterator<char>(formated.ToList(), IterationOptions.ToCollectionEnd);
+            tokens = new List<Token>();
+        }
+
+        private Dictionary<TokenType, char> OperationTypes { get; } =
+            new Dictionary<TokenType, char>
+            {
+                { TokenType.Minus, '-' },
+                { TokenType.Plus, '+' },
+                { TokenType.Division, '/' },
+                { TokenType.Multiply, '*' }
+            };
+
+        private const string operationChars = "[-+/*]";
+        private const string operandChars = "[.a-zA-Z\\d]";
+
+        private readonly Iterator<char> iterator;
+        private readonly List<Token> tokens;
+
+        public List<Token> TokenizeExpression()
+        {
+            while (!EndOfExpression())
+            {
+                tokens.Add(ToToken(iterator.CurrentElement));
+                iterator.MoveForward();
+            }
+
+            return tokens;
+        }
+
+        private Token ToToken(char currentChar) => currentChar.ToString() switch
+        {
+            var operation when Regex.IsMatch(operation, operationChars) => GenerateOperationToken(),
+            var operand when Regex.IsMatch(operand, operandChars)  => GenerateOperandToken(),
+            "(" => new Token(currentChar.ToString(), TokenType.LBrace, 3),
+            ")" => new Token(currentChar.ToString(), TokenType.RBrace, 3),
+            _ => throw new Exception("Unknown expression character")
+        };
+
+        private Token GenerateOperationToken()
+        {
+            var type = OperationTypes.First(operationType => operationType.Value == iterator.CurrentElement);
+            var priority = (type.Value == '+') || (type.Value == '-') ? 1 : 2;
+
+            return new Token(iterator.CurrentElement.ToString(), type.Key, priority);
+        }
+
+        private Token GenerateOperandToken()
+        {
+            var operand = new StringBuilder();
+
+            while (Regex.IsMatch(iterator.CurrentElement.ToString(), operandChars))
+            {
+                operand.Append(iterator.CurrentElement);
+                iterator.MoveForward();
+            }
+
+            if(iterator.NextElement is not default(char) || EndsWithBrackets()) 
+                iterator.MoveBackward();
+
+            return new Token(operand.ToString(), TokenType.Operand, 0);
+        }
+
+        private bool EndsWithBrackets() => iterator.CurrentElement is ')' &&
+            iterator.NextElement is default(char);
+
+        private bool EndOfExpression() => iterator.CurrentElement is default(char);
+    }
+}
---
+++ ASTBuilder/TreeBuilder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Utility;
+
+namespace ASTBuilder
+{
+    public class Node
+    {
+        public Node(Token token)
+        {
+            Token = token;
+        }
+
+        public readonly Token Token;
+        public Node LeftChild { get; set; }
+        public Node RightChild { get; set; }
+    }
+
+    public class TreeBuilder
+    {
+        public TreeBuilder(List<Group> groups)
+        {
+            this.groups = groups;
+        }
+
+        private readonly List<Group> groups;
+
+        public Node Build()
+        {
+            var head = groups.Last();
+
+            var root = new Node(head.Operation)
+            {
+                LeftChild = CreateNodeFrom(head.FirstOperand),
+                RightChild = CreateNodeFrom(head.SecondOperand)
+            };
+
+            return root;
+        }
+
+        private Node CreateNodeFrom(Token token) => token.type is TokenType.Group ?
+            CreateNodeFromGroupBy(token) : new Node(token);
+
+        private Node CreateNodeFromGroupBy(Token token)
+        {
+            var tokenGroup = groups.Find(group => group.name == token.value);
+
+            var node = new Node(tokenGroup.Operation)
+            {
+                LeftChild = CreateNodeFrom(tokenGroup.FirstOperand),
+                RightChild = CreateNodeFrom(tokenGroup.SecondOperand)
+            };
+
+            return node;
+        }
+    }
+}
